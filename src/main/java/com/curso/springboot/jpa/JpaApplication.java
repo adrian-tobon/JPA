83c84eafc0e53429e778dabdfb44f10a5180765d
ctobonpe <ctobonpe@gmail.com>
2025-06-03 06:24:32 -0500

    ejemplos query method y @Query

--- src/main/java/com/curso/springboot/jpa/JpaApplication.java
+++ src/main/java/com/curso/springboot/jpa/JpaApplication.java
@@ -1,6 +1,7 @@
 package com.curso.springboot.jpa;
 
 import java.util.List;
+//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,14 +23,46 @@ public class JpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		findOne();
+	
+	}
+	
+	public void findOne() {
+		//Person person = personRepository.findById(2l).orElseThrow();
+		/*Person person = null;
+		Optional<Person> optional = personRepository.findById(8l);
+		if(optional.isPresent()) {
+			person = optional.get();
+			System.out.println(person);
+		}else {
+			System.out.println("la persona no se encuentra en la base de datos");
+		}*/
+			
+		//personRepository.findById(8l).ifPresent(person -> System.out.println(person));
+		//personRepository.findById(8l).ifPresent(System.out::println);
+		personRepository.findOne(2l).ifPresent(System.out::println);
+		personRepository.findByName("Andres").ifPresent(System.out::println);
+		personRepository.findByNameContaining("se").ifPresent(System.out::println);
+		personRepository.findByNameIgnoreCaseContaining("an").ifPresent(System.out::println);
+		personRepository.buscarPorNombreCoincidencia("ri").ifPresent(System.out::println);
+		
+	}
+	
+	
+	public void list() {
 		
 		//List<Person> persons = (List<Person>)personRepository.findAll();
 		//List<Person> persons = (List<Person>)personRepository.findByProgrammingLanguage("Java");
 		List<Person> persons = (List<Person>)personRepository.findByProgrammingLanguageAndName("Java","Andres");
 		//List<Person> persons = (List<Person>)personRepository.buscarPorProgrammingLanguage("Java");
+				
+		persons.stream().forEach(person -> System.out.println(person));		
 		
-		persons.stream().forEach(person -> System.out.println(person));
+		List<Object[]> personsValues = personRepository.obtenerPersonData();
 		
-	}
+		personsValues.stream().forEach(person -> {
+			System.out.println(person[0] + " es experto en " + person[1]);			
 
+		});		
+	}
 }
--- src/main/java/com/curso/springboot/jpa/repositories/PersonRepository.java
+++ src/main/java/com/curso/springboot/jpa/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.curso.springboot.jpa.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,9 +13,25 @@ public interface PersonRepository extends CrudRepository<Person, Long>{
 	
 	List<Person> findByProgrammingLanguage(String programmingLanguage);	
 	
-	List<Person> findByProgrammingLanguageAndName(String programmingLanguage,String name);	
+	List<Person> findByProgrammingLanguageAndName(String programmingLanguage,String name);
+	
+	Optional<Person> findByName(String name);
+	
+	Optional<Person> findByNameContaining(String name);
+	
+	Optional<Person> findByNameIgnoreCaseContaining(String name);
 	
 	@Query("select p from Person p where p.programmingLanguage = ?1")
 	List<Person> buscarPorProgrammingLanguage(String programmingLanguage);
-
+	
+	@Query("select p.name, p.programmingLanguage from Person p")
+	List<Object[]> obtenerPersonData();
+	
+	@Query("select p from Person p where p.id = ?1")
+	Optional<Person> findOne(Long id);
+	
+	
+	@Query("select p from Person p where p.name like %?1%")
+	Optional<Person> buscarPorNombreCoincidencia(String pattern);
+	
 }
